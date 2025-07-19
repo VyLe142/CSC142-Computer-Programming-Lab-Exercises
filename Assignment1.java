bc4a8041754d2ee3e70a09a9d563c8d29e51cd09
VyLe142 <nguyenkimvyle@gmail.com>
2025-07-18 19:16:42 -0700

    Initial commit

--- Assignment1.java
+++ Assignment1.java
@@ -1,6 +1,6 @@
-public public class Assignment1 {
-     public static void main(String[] args) {  
+public class Assignment1 {
+    public static void main(String[] args) {
         System.out.println("CSC142 - Intro to Java");
-        System.out.println("I learned how to download and use Java"); 
+        System.out.println("I learned how to download and use Java");
     }
 }
