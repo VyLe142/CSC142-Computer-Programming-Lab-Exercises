dd6c671cd48da6a9556c1c76e61bae527d48535d
VyLe142 <nguyenkimvyle@gmail.com>
2025-07-25 15:25:43 -0700

    Initial commit

--- Quiz2.java
+++ Quiz2.java
@@ -1,3 +1,4 @@
+import java.awt.geom.CubicCurve2D;
 import java.util.Scanner;
 public class Quiz2 {
     public static void main(String[] args) {
@@ -6,11 +7,15 @@ public class Quiz2 {
         double mass_of_object = input.nextInt();
         double weight = mass_of_object * 9.8;
         if (weight > 1000){
-            System.out.println("Too heavy");
+            System.out.println("too heavy");
      
        }
        else if (weight < 10){
-        System.out.println("Too light");
+        System.out.println("too light");
+       }
+       else{
+        System.out.println("nothing");
+
        }
        input.close();
        
